2128c3895ba55474079fd1d2ab018ec4a1910442
makwana-jaydip <jaydipmm97@gmail.com>
2025-05-04 16:32:49 +0000

    Added a test-cases/spec for simple add operation

--- spec/services/string_calculator_spec.rb
+++ spec/services/string_calculator_spec.rb
@@ -5,5 +5,13 @@ RSpec.describe StringCalculator do
     it 'returns 0 for an empty string' do
       expect(described_class.add('')).to eq(0)
     end
+
+    it 'returns the number itself when a single number is provided' do
+      expect(described_class.add('1')).to eq(1)
+    end
+
+    it 'returns the sum of two numbers separated by a comma' do
+      expect(described_class.add('1,2')).to eq(3)
+    end
   end
 end
