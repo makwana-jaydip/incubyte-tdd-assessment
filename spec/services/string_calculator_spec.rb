876a2c356feb9fc4e92871bb174849dbceeaed9e
makwana-jaydip <jaydipmm97@gmail.com>
2025-05-04 16:42:57 +0000

    Added test-cases for more than 2 numbers in comma separted

--- spec/services/string_calculator_spec.rb
+++ spec/services/string_calculator_spec.rb
@@ -1,17 +1,22 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe StringCalculator do
-  describe '.add' do
-    it 'returns 0 for an empty string' do
-      expect(described_class.add('')).to eq(0)
+  describe ".add" do
+    it "returns 0 for an empty string" do
+      expect(described_class.add("")).to eq(0)
     end
 
-    it 'returns the number itself when a single number is provided' do
-      expect(described_class.add('1')).to eq(1)
+    it "returns the number itself when a single number is provided" do
+      expect(described_class.add("1")).to eq(1)
     end
 
-    it 'returns the sum of two numbers separated by a comma' do
-      expect(described_class.add('1,2')).to eq(3)
+    it "returns the sum of two numbers separated by a comma" do
+      expect(described_class.add("1,2")).to eq(3)
+    end
+
+    it "returns the sum of multiple numbers separated by commas" do
+      expect(described_class.add("1,2,3")).to eq(6)
+      expect(described_class.add("4,5,6,7")).to eq(22)
     end
   end
 end
